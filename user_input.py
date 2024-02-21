b822183fd7f58842812bc5a719b83ba6aa2dd273
Shanaly34 <alfredbaraka287@gmail.com>
2024-02-21 13:28:56 +0300

    First

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Please enter Your Name: ")
+age = input("Please enter Your Age: ")
+location = input("Please enter Your Location: ")
+print("Hello {}, you are {} years old and live in {}".format(name, age, location))
